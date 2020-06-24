521f52baec2378ffa52d095ae43a70c5066b85ff
Adam Topaz <github@adamtopaz.com>
2020-06-23 20:31:29 -0600

    Proved inclusive and monotone axioms

--- src/pregeom/pushforward.lean
+++ src/pregeom/pushforward.lean
@@ -11,32 +11,35 @@ and the requirement that ∀ t ∈ T, f ⁻¹({t}) is closed, we can define a cl
 letting cl U := f (cl f ⁻¹ (U))
 -/
 
-
 namespace pregeom
 namespace pushforward
 
 open function
 
-variables {T : Type*} {S : Type*} [has_cl S] (f : S → T)
+variables {T : Type*} {S : Type*} (f : S → T)
 include f
 
-def has_closed_fibers := ∀ t, f⁻¹' ({t}) = cl (f⁻¹' {t})
+def has_closed_fibers [has_cl S] := ∀ t, f⁻¹' ({t}) = cl (f⁻¹' {t})
 
-def has_cl_instance (f : S → T) : has_cl T := ⟨λ (U : set T), f '' (cl (f ⁻¹' U))⟩
+def has_cl_instance [has_cl S] (f : S → T) : has_cl T := ⟨λ (U : set T), f '' (cl (f ⁻¹' U))⟩
 
-def pregeom_instance [pregeom S] (cf : has_closed_fibers f) (sf : surjective f): pregeom T :=
-{
-  inclusive := 
+def pregeom_instance [pregeom S] (cf : has_closed_fibers f) (sf : surjective f) : pregeom T :=
+{ inclusive := λ U u hu,
   begin
-    intros U u hu,
-    unfold cl,
+    change _ ∈ f '' _,
+    rcases sf u with ⟨v,rfl⟩,
+    exact ⟨v,inclusive hu,rfl⟩,
   end,
-  monotone := sorry,
+  monotone := λ A B h,
+  begin
+    replace h := @set.monotone_preimage _ _ f _ _ h,
+    replace h := monotone h,
+    exact @set.monotone_image _ _ f _ _ h,
+  end,
   idempotent := sorry,
   exchange := sorry,
   finchar := sorry,
-  ..show has_cl T, by exact has_cl_instance f,
-}
+  ..show has_cl T, by exact has_cl_instance f }
 
 end pushforward
 end pregeom
